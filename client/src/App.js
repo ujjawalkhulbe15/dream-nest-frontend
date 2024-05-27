77be985ace06ad133ab43bc0f6a4457fd2acba99
Ujjawalkhulbe2001 <131869096+Ujjawalkhulbe2001@users.noreply.github.com>
2024-05-28 01:52:58 +0530

    hashrouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
@@ -15,21 +15,24 @@ import SearchPage from "./pages/SearchPage";
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/create-listing" element={<CreateListing />} />
           <Route path="/properties/:listingId" element={<ListingDetails />} />
-          <Route path="/properties/category/:category" element={<CategoryPage />} />
+          <Route
+            path="/properties/category/:category"
+            element={<CategoryPage />}
+          />
           <Route path="/properties/search/:search" element={<SearchPage />} />
           <Route path="/:userId/trips" element={<TripList />} />
           <Route path="/:userId/wishList" element={<WishList />} />
           <Route path="/:userId/properties" element={<PropertyList />} />
           <Route path="/:userId/reservations" element={<ReservationList />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
